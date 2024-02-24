79957a405397ae2b408ee09022c74cfb9727f8d8
Ranjan Giri <68289676+rjn-dev@users.noreply.github.com>
2024-02-24 09:07:58 +0530

    articles width fix

--- components/KnowMoreComp/index.tsx
+++ components/KnowMoreComp/index.tsx
@@ -15,7 +15,7 @@ const KnowMoreComp = ({ heading, articleData, viewMore, route }: KnowMoreProps) 
     return (
         <div className={styles.kumCard}>
             <p className={styles.kumCardTitle}>{heading}</p>
-            <div className='md:h-[465px] w-[800px] m-auto scrollable-content'>
+            <div className='md:h-[465px] md:w-[800px] m-auto scrollable-content'>
             {
                 articleData && articleData?.payload?.sort((a:any, b:any) => {
                     let c: any = new Date(a.date);
