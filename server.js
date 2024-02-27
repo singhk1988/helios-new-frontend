903d4ba808597727e6c8489349d0ef2912bada13
Aditya Toshniwal <aditya.toshniwal@enterprisedb.com>
2024-02-27 17:34:45 +0530

    Remove cross-env

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const port =process.env.PORT || 3000
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
+console.log('dev', dev);
 
 app.prepare().then(() => {
   createServer(async (req, res) => {
